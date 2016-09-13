574cd6a7c209749d660b0f7ad011c9ac758577ab
Kayla Nussbaum <kxn3867@rit.edu>
2016-09-13 18:43:55 -0400

    creating all files

---
+++ get_target.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="standard.css">
+</head>
+<body id="bg1">
+  <h1>GET Interaction</h1>
+  <br>
+  <h2>PHP Echo</h2>
+  <br>
+  <p>Userame: <?php echo $_GET['username']; ?></p>
+  <p>Age: <?php echo $_GET['age']; ?></p>
+  <p>State: <?php echo $_GET['state']; ?></p>
+  <p>Gender: <?php echo $_GET['gender']; ?></p>
+  <br><br>
+  <a href="./">Back to Welcome Screen</a>
+</body>
+</html>
+
+
---
+++ post_target.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="standard.css">
+</head>
+<body id="bg1">
+  <h1>POST Interaction</h1>
+  <h2>PHP Echo</h2>
+  <p>Username: <?php echo $_POST['username']; ?></p>
+  <p>Age: <?php echo $_POST['age']; ?></p>
+  <p>State: <?php echo $_POST['state']; ?></p>
+  <p>Gender: <?php echo $_POST['gender']; ?></p>
+  <br><br><br>
+  <a href="./">Back to Welcome Screen</a>
+</body>
+</html>
+
